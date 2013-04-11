68c54b992ae8fe32827b4cda8877a3679c384b0f
Maciej <maciej.zurad@gmail.com>
2013-04-11 09:32:53 +0200

    SRAM Testing

--- firmware/main.c
+++ firmware/main.c
@@ -2,8 +2,8 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 #include <util/atomic.h>
-//#include <malloc.h>
 #include <stdlib.h>
+
 
 #define CLOCK (1<<PB1)
 #define DATA (1<<PB2)
@@ -12,11 +12,14 @@
 
 #define NUM_TLC5947 5
 
+#define ANGLE 120
 
 volatile uint8_t need_to_run, set_white;
 volatile uint8_t j;
 volatile uint8_t buffer[120];
-volatile uint8_t rpm_counter = 0;
+volatile uint8_t translated_buffer[180];
+volatile uint8_t last_rpm[3];
+volatile uint8_t last_rpm_index = 0;
 volatile uint8_t angle_choose = 0;
 
 
@@ -37,6 +40,17 @@ inline uint16_t translate(uint8_t a) {
     return (a * 4095) / 255;
 }
 
+void send_debug_number_16bit( uint16_t num){
+    uint8_t i;
+    char num_s[6];
+    itoa(num, num_s, 10);
+    for (i=0; i<strlen(num_s); ++i) {
+        USART_Transmit(num_s[i]);
+    }
+    USART_Transmit('\n');
+    USART_Transmit('\r');
+}
+
 void send_debug_number( uint8_t num) {
     uint8_t i;
     char num_s[4];
@@ -53,10 +67,15 @@ void USART_Transmit( uint8_t data ) {
     UDR1 = data;
 }
 
+inline uint8_t calc_rpm_avg(){
+    return (last_rpm[0] + last_rpm[1] + last_rpm[2]) / 3 ;
+}
+
 void send_translate() {
     
     static uint8_t mode = 1, to_send = 0;
-    uint16_t tmp=0, i=0;
+    uint16_t tmp=0;
+    uint8_t i=0;
     
     for (i = 0; i < 120; i++) {
         tmp = translate(buffer[i]);
@@ -71,6 +90,34 @@ void send_translate() {
             send_led(to_send);
             
             send_led((tmp << 8) >> 8);
+        }
+        mode = !mode;
+    }
+}
+
+void send_translated_buffer(){
+    uint8_t i;
+    for (i=0; i<180; ++i) {
+        send_led(translated_buffer[i]);
+    }
+}
+
+void translate_buffer() {
+    static uint8_t mode = 1, to_translate = 0;
+    uint16_t tmp=0;
+    uint8_t i=0, j=0;
+    
+    for (i=0, j=0; i < 120; i++, j++) {
+        tmp = translate(buffer[i]);
+        // wysylka prawych 8 bitow
+        if (mode) {
+            translated_buffer[j] = (uint8_t) (tmp >> 4);
+            to_translate = ((tmp << 12) >> 8);
+        } else {
+            to_translate |= tmp >> 8;
+            translated_buffer[j] = to_translate;
+            ++j;
+            translated_buffer[j] = ((tmp << 8) >> 8);
         }
         
         mode = !mode;
@@ -118,10 +165,9 @@ void send_translate() {
 //}
 
 ISR(INT7_vect) {
-    rpm_counter++;
+    last_rpm[last_rpm_index]++;
     TCNT2 = 0;
 }
-
 
 // Odbior danych z uarta
 //ISR(USART3_RX_vect)
@@ -132,15 +178,18 @@ ISR(INT7_vect) {
 //}
 
 ISR(TIMER1_COMPA_vect) {
-    TCNT2 = 0;
-    OCR2A = 15625 / 120 / rpm_counter;
+    ATOMIC_BLOCK(ATOMIC_FORCEON) {
+        TCNT2 = 0;
+        OCR2A = 15625 / ANGLE / calc_rpm_avg();
+    }
     
+    send_debug_number(calc_rpm_avg());
     
-    send_debug_number(rpm_counter);
-    rpm_counter = 0;
+    last_rpm_index++;
+    last_rpm_index %= 3;
     
-    if (rpm_counter>20){
-        
+    if (calc_rpm_avg() > 20){
+        TCCR2B = (1<<CS12) | (1<<CS10);
     }
     
 //    for (i = 0; i < 120; i++) {
@@ -165,7 +214,7 @@ ISR(TIMER2_COMPA_vect) {
 
 int main(void)
 {
-    sei();
+    //sei();
     
     UCSR1B = (1<<RXEN1) | (1<<TXEN1);// | (1<<RXCIE1);// | (1<<TXCIE1); //0x18;      //reciever enable , transmitter enable
     UBRR1H = 0;
@@ -202,17 +251,48 @@ int main(void)
     
     //Second 16-bit timer - 1 interrupt per angle
     TCCR2A = 0;
-    TCCR2B |= (1<<WGM12); // choosing 
-    OCR2A = 15626 / 120;
+    TCCR2B |= (1<<WGM12); // choosing Timer mode - CTC
+    OCR2A = 15626 / ANGLE;
     TIMSK2 |= (1<<OCIE2A) | (1<<OCIE2B); //Set TOIE bit
 
+    //Enable xmem
+    XMCRA |= (1<<SRE);
+    //Using whole port C
+    XMCRB = 0;
+    
     uint8_t i;
     
     for (i = 0; i < 180; i++)
         send_led(0);
     commit();
     
-    while (1) {}
+    //Fill buffer with red diodes only
+    for (i=0; i < 120; i++) {
+        buffer[i] = (i%3==0) ? 255 : 0;
+    }
+    
+    translate_buffer();
+    send_translated_buffer();
+    
+    commit();
+    
+    uint8_t* temp = malloc(56000);
+    uint16_t j;
+    
+
+
+    while (1) {
+    
+        _delay_ms(1000);
+        for (j=0; j<(56000\;p1`int1; ++j) {
+            temp[j] = 46;
+            send_debug_number_16bit(j);
+            USART_Transmit(' ');
+            send_debug_number(temp[j]);
+            USART_Transmit('\n');
+            USART_Transmit('\r');
+        }
+    }
     
     return 0;
 }
